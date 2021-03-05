006e479c59afcd237940a019b190e9a31a089b01
xudufy <xudufy@qq.com>
2021-03-05 17:23:22 -0500

    tested_numAttack

--- src/Board.java
+++ src/Board.java
@@ -12,7 +12,7 @@ public class Board {
     //The following two are redundant.
     //After you change one of them, use syncWithXXX function to update the other one.
     public int[][] board = null;
-    public ArrayList<QueenEntry> queens;
+    public ArrayList<QueenEntry> queens; // start from 1, end at numQueen, size is numQueen+1, queens[0] is never used.
 
     //Flat version of board is used for generate random board.
     //The following functions are for the transforms of flat->board, queens->board, board->queens, board->flat.
@@ -35,9 +35,11 @@ public class Board {
         return out;
     }
 
+    //weightList index from 0 to numQueen-1.
     public static ArrayList<QueenEntry> board2Queens(int[][] board, int[] weightList) {
         int n = board.length;
         ArrayList<QueenEntry> out = new ArrayList<>();
+        out.add(QueenEntry.of(0,0,-1)); //dummy out[0];
         for (int j : weightList) {
             out.add(QueenEntry.of(0, 0, j));
         }
@@ -58,7 +60,7 @@ public class Board {
 
     public static int[][] queens2Board(ArrayList<QueenEntry> queens, int size) {
         int[][] out = new int[size][size]; // init to 0 according to java spec.
-        for (int i=0; i<queens.size(); i++) {
+        for (int i=1; i<=queens.size(); i++) {
             out[queens.get(i).row()][queens.get(i).col()] = i;
         }
         return out;
@@ -114,13 +116,14 @@ public class Board {
     //out[i] is the possible moves for ith queen.
     public ArrayList<ArrayList<QueenEntry>> allPossibleMoves() {
         ArrayList<ArrayList<QueenEntry>> out = new ArrayList<>();
-        for (int i=0; i<numQueen; i++) {
+        for (int i=1; i<=numQueen; i++) {
             out.add(allPossibleMovesForQueen(i));
         }
         return out;
     }
 
     public ArrayList<QueenEntry> allPossibleMovesForQueen(int queenIndex) {
+        assert queenIndex > 0;
         int currentRow = queens.get(queenIndex).row();
         int currentCol = queens.get(queenIndex).col();
         int weight = queens.get(queenIndex).weight();
@@ -140,21 +143,26 @@ public class Board {
     }
 
     //don't rely on this, bad performance.
+    //weight in newPos is not used.
     public boolean checkValidSingleMove(int queenIndex, QueenEntry newPos) {
         return allPossibleMovesForQueen(queenIndex).stream().anyMatch(n->n.equals(newPos));
     }
 
-    //Single move must be valid move.
+    //Single move must be valid move, not checked in this function.
+    //weight in newPos is not used.
     public int getSingleMoveCost(int queenIndex, QueenEntry newPos) {
+        assert queenIndex>0;
         return getSingleMoveCost(queens.get(queenIndex), newPos);
     }
 
-    //Single move must be valid move.
+    //Single move must be valid move, not checked in this function.
+    //the weight in 'to' is not used.
     public static int getSingleMoveCost(QueenEntry from, QueenEntry to) {
         int step = Math.max(Math.abs(to.row()- from.row()), Math.abs(to.col() - from.col()));
         return step * from.weight() * from.weight();
     }
 
+    //the weight in 'to' is not used.
     private static int getQueenEntireMoveCost(QueenEntry from, QueenEntry to) {
         int dx = Math.abs(to.row() - from.row());
         int dy = Math.abs(to.col() - from.col());
@@ -167,14 +175,15 @@ public class Board {
         assert ref.numQueen == this.numQueen;
         assert ref.size == this.size;
         int out = 0;
-        for (int i=0; i< this.numQueen; i++) {
+        for (int i = 1; i <= this.numQueen; i++) {
             out += getQueenEntireMoveCost(ref.queens.get(i), this.queens.get(i));
         }
         return out;
     }
 
-    //return the new Board after the move.
+    //return the new Board after the move. The weight in newPos is not used.
     public Board moveAQueen(int queenIndex, QueenEntry newPos) {
+        assert queenIndex>0;
         Board out = Board.ofBoard(this);
         QueenEntry n = newPos.setWeight(out.queens.get(queenIndex).weight());
         out.board[out.queens.get(queenIndex).row()][out.queens.get(queenIndex).col()] = 0;
@@ -189,6 +198,7 @@ public class Board {
         int[] mainDiagCount = new int[size * 2];
         int[] antiDiagCount = new int[size * 2];
         for (QueenEntry q: queens) {
+            if (q.equals(QueenEntry.of(0,0,-1))) continue; // for dummy queens[0].
             rowCount[q.row()] ++;
             colCount[q.col()] ++;
             mainDiagCount[q.col()-q.row()+size]++;
@@ -198,29 +208,44 @@ public class Board {
         int out = 0;
         for (int c:rowCount) {
             if (c!=0) {
-                out += c-1;
+                out += c*(c-1)/2;
             }
         }
 
         for (int c:colCount) {
             if (c!=0) {
-                out += c-1;
+                out += c*(c-1)/2;
             }
         }
 
         for (int c:mainDiagCount) {
             if (c!=0) {
-                out += c-1;
+                out += c*(c-1)/2;
             }
         }
 
         for (int c:antiDiagCount) {
             if (c!=0) {
-                out += c-1;
+                out += c*(c-1)/2;
             }
         }
 
         return out;
+    }
+
+    public static void main(String[] args) {
+        int[][] startboard = {
+                {1,0,0,0,0,0,0,0},
+                {0,2,0,0,0,0,0,0},
+                {0,0,3,0,0,0,0,0},
+                {0,0,0,4,0,0,0,0},
+                {0,0,0,0,5,0,0,0},
+                {0,0,0,0,0,6,0,0},
+                {0,0,0,0,0,0,7,0},
+                {0,0,9,0,0,0,0,8}
+        };
+        Board b = new Board(startboard, new int[9]);
+        System.out.println(b.findNumAttacks());
     }
 
 }
