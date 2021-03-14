5a4039d7c457c7e7f830ea84d35749926cfc8865
JasonMartino720 <jasonamartino@gmail.com>
2021-03-13 20:51:40 -0500

    Finished Project One

--- src/HillClimbing.java
+++ src/HillClimbing.java
@@ -1,18 +1,21 @@
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class HillClimbing {
 
     public static Random rand;
 
-    public static final int BoardSize = 16;
+    public static final int BoardSize = 256;
     public static final int NumQueens = BoardSize * 9 / 8;
-    public static final int maxAllowableResets = 5;
+    public static final int maxAllowableResets = 2;
+    public static final int maxSideMoves = 10;
     public static double Tmp,StartingTmp = 10000;
-    public static final double TempDegradation = 0.5;
+    public static final double TempDegradation = 0.25;
     public static final double minAllowableTemp = 1;
 
     public static Board currentBoard, refBoard, bestBoard;
-    public static int currentCost, lowestCost, thisLowestCost, startingCost;
+    public static int currentCost, lowestCost, thisLowestCost, startingCost, sideMoves = 0;
+
 
     //Calculate Heuristic Cost of Given Board
     public static int calculateCost(Board board) {
@@ -26,6 +29,7 @@ public class HillClimbing {
 
     public static Board nextBoard(Board board) {
         int boardCost = calculateCost(board);
+        //System.out.println("Current Cost " + boardCost);
         ArrayList<ArrayList<QueenEntry>> allMoves = board.allPossibleMoves();
 
         //Create list of Queen IDs (1-N) and randomize it
@@ -43,78 +47,93 @@ public class HillClimbing {
             //Iterate through all possible moves for "queenID-th" queen
             for (QueenEntry i : subsetMoves) {
                 Board tmpBoard = board.moveAQueen(queenID, i);
+                int tmpCost = calculateCost(tmpBoard);
+                Tmp *= TempDegradation;
                 //If this board represents a lower cost accept it, if not accept it with some probability
-                if (calculateCost(tmpBoard) < boardCost){
-                    //System.out.println("Found Better " + Tmp);
-                    Tmp *= TempDegradation;
+                if ((boardCost-tmpCost)>15){
+//                    System.out.println("Found Better " + Tmp);
                     return tmpBoard;
                 }
-                else {
-                    if ((Math.random() < simulatedAnnealing(boardCost, calculateCost(tmpBoard), Tmp))&&(Tmp > minAllowableTemp)){
-                        //System.out.println("Took Backwards " + Tmp);
-                        Tmp *= TempDegradation;
+                else if(((boardCost-tmpCost) < 15) && ((boardCost-tmpCost) >= 0) && sideMoves < maxSideMoves) {
+                    sideMoves +=1;
+//                    System.out.println("Took Sideways " + Tmp);
+                    return tmpBoard;
+                }
+                else{
+                    if((Math.random() < simulatedAnnealing(boardCost, tmpCost, Tmp))&&(Tmp > minAllowableTemp)) {
+//                        System.out.println("Took Backwards " + Tmp);
                         return tmpBoard;
                     }
                 }
-
             }
         }
-        Tmp *= TempDegradation;
         //null is returned if no better move was discovered
         return null;
     }
 
     public static void main(String[] args) {
-        double avgTime = 0, avgCost =0;
-        int numRuns = 500;
-        for(int runs = 0; runs < numRuns; runs++)
-        {
-            //First Choice Rn
-            refBoard = Board.ofRandomBoard(8, NumQueens);
+        long startTime = 0, endTime  = 0, elapsedTime, elapsedTimeMs, elapsedTimeS;
+        long avgTime = 0,  totalTime = 0;
+        int avgCost = 0,totalCost = 0, avgStartCost = 0, totalStartCost = 0;
+        int maxRuns = 3;
+
+        for(int runs = 0; runs < maxRuns; runs++) {
+            refBoard = Board.ofRandomBoard(BoardSize, NumQueens);
             currentBoard = refBoard;
             currentCost = calculateCost(currentBoard);
             startingCost = currentCost;
+            totalStartCost += startingCost;
             System.out.println("Starting Cost " + startingCost);
             lowestCost = currentCost;
+            Tmp = StartingTmp;
 
-            long start = System.nanoTime();
-
-            for(int i =0; i< maxAllowableResets; i++) {
+            for (int i = 0; i < maxAllowableResets; i++) {
+                if(i==0){
+                    startTime = System.nanoTime();
+                }
 
                 while (true) {
-                    if (nextBoard(currentBoard) != null) {
-                        System.out.println(nextBoard(currentBoard));
-                        currentBoard = nextBoard(currentBoard);
+                    Board tmpBoard = nextBoard(currentBoard);
+                    if (tmpBoard != null) {
+                        currentBoard = tmpBoard;
                     } else {
                         thisLowestCost = calculateCost(currentBoard);
-
                         break;
                     }
                 }
 
-                if(thisLowestCost < lowestCost){
+                if (thisLowestCost < lowestCost) {
                     lowestCost = thisLowestCost;
                     bestBoard = currentBoard;
                     System.out.println("New Lowest " + lowestCost);
                 }
 
-                if(i < maxAllowableResets){
+                if (i < maxAllowableResets) {
                     //Reset if we're going to go through again
                     currentBoard = refBoard;
                     Tmp = StartingTmp;
                 }
             }
-            long end = System.nanoTime();
-            double elapsedTime = ((end-start)/Math.pow(1,6));
+            endTime = System.nanoTime();
+            elapsedTime = (endTime - startTime);
+            elapsedTimeMs = TimeUnit.MILLISECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
+            elapsedTimeS = TimeUnit.SECONDS.convert(elapsedTime, TimeUnit.NANOSECONDS);
+            totalTime += elapsedTimeS;
+            totalCost += lowestCost;
             System.out.println("Final Board Cost: " + lowestCost);
-            System.out.println("Elapsed Time in ms: " + elapsedTime);
+//            System.out.println("Start Time " + startTime);
+//            System.out.println("End Time " + endTime);
+//            System.out.println("Elapsed Time in ns: " + elapsedTime);
+//            System.out.println("Elapsed Time in ms: " + elapsedTimeMs);
+            System.out.println("Elapsed Time in S " + elapsedTimeS);
 
-            avgTime += elapsedTime;
-            avgCost += lowestCost;
         }
-        avgCost = avgCost / numRuns;
-        avgTime = avgTime / numRuns;
-        System.out.println("Average Cost: " + avgCost);
-        System.out.println("Average Runtime: " + avgCost);
+        avgStartCost = totalStartCost / maxRuns;
+        avgCost = totalCost / maxRuns;
+        avgTime = totalTime / maxRuns;
+        System.out.println("Average Starting Cost " + avgStartCost);
+        System.out.println("Average Cost " + avgCost);
+        System.out.println("Average Time " + avgTime);
+
     }
 }
